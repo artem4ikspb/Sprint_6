5a6c54698f43fbca62bb0876224714fd026ed772
ArtemK <artem4ik.spb@yandex.ru>
2025-06-08 16:44:04 +0300

    delete duplicates 2

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -8,7 +8,6 @@ class MainPageLocators:
     QUESTION_LOCATOR_LAST = By.XPATH, './/div[@id="accordion__heading-7"]'
 
     FAQ_MENU = By.CSS_SELECTOR, "div.Home_FourPart__1uthg"
-    SCOOTER_LOGO = By.CSS_SELECTOR, "a.Header_LogoScooter__3lsAR"
 
     ORDER_TOP_BUTTON = By.CSS_SELECTOR, "div.Header_Nav__AGCXC button.Button_Button__ra12g"
     ORDER_BOTTOM_BUTTON = By.CSS_SELECTOR, "div.Home_FinishButton__1_cWm button.Button_Button__ra12g"
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,7 @@
 import allure
-from locators.main_page_locators import MainPageLocators
 from locators.general_locators import GeneralLocators
+from locators.main_page_locators import MainPageLocators
 from locators.order_page_locators import OrderPageLocators
-
-
 from pages.base_page import BasePage
 
 
